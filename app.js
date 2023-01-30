e4afdb6fa82faaddfe8635ceddcf82eb64fd6fff
Tomas Gongora <tomasgongora@hotmail.com>
2023-01-30 06:08:15 -0300

    Subiendo trabajo practico

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ("express");
+const app = express();
+const path = require ("path")
+const PORT = 3000;
+
+const mainRouter = require ("./routers/main");
+
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use("/", mainRouter);
+
+
+app.listen(PORT, () => console.log(`
+Server listen in port ${PORT}
+http://localhost:${PORT}
+`))
